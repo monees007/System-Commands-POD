5d5a9540cabb6057de7fe4c011dd151a8d41fd38
monees007 <no1knows@protocolGithub>
2022-05-20 15:44:58 +0530

    Website is ready.

--- index.py
+++ index.py
@@ -3,16 +3,16 @@ from flask import render_template
 
 app = Flask(__name__)
 weekl=[]
-for i in range(1,8):
+for i in range(1,9):
     weekl.append(f'Week {i}')
 @app.route('/')
 def index():
-    return render_template('README.html',weekl=weekl)
+    return render_template('README.html',weekl=weekl,leng=len(weekl))
 
 
 @app.route('/notes/<i>')
-def notes():
-    return render_template('Week {i}')
+def notes(i):
+    return render_template(f'Week{str(int(i)+1)}.html',weekl=weekl,leng=len(weekl))
 
 if __name__ == '__main__':
     app.run(debug=True)
